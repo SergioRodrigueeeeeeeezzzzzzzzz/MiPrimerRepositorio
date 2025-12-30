b604d89ad14c7c92fa7f651114075b400d821542
Sergio Rodriguez Mayor <srodriguezmayor@educa.madrid.org>
2025-12-30 19:22:48 +0100

    Delete src/MiPrimerRepositorioJava directory

--- src/MiPrimerRepositorioJava/MiPrimerRepositorioJava.java
+++
@@ -1,8 +0,0 @@
-package MiPrimerRepositorioJava;
-
-public class MiPrimerRepositorioJava {
-	public static void main(String[]args) {
-		System.out.println("Prueba");
-		System.out.println("Pruaba1");
-	}
-}
